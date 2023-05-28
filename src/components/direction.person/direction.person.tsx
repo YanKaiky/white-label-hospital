9fa4faac5500a90cb28f609473c73999b74a1380
Yan Kaiky <yankaikys@gmail.com>
2023-05-28 02:36:56 -0300

    feat(DirectionPerson): add fontSize

--- src/components/direction.person/direction.person.tsx
+++ src/components/direction.person/direction.person.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Avatar, Box, Card, CardContent, Typography } from "@mui/material";
+import { Avatar, Box, Card, CardContent, Typography, useMediaQuery, useTheme } from "@mui/material";
 
 interface IDirectionPersonProps {
   name: string;
@@ -9,6 +9,13 @@ interface IDirectionPersonProps {
 }
 
 export const DirectionPerson: FC<IDirectionPersonProps> = ({ image, job, name, bgcolor }) => {
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'));
+  const lgDown = useMediaQuery(theme.breakpoints.down('lg'));
+  const xsDown = useMediaQuery(theme.breakpoints.down('xs'));
+  const xlDown = useMediaQuery(theme.breakpoints.down('xl'));
+
   return (
     <Card sx={{ bgcolor: '#E9E9E9', padding: 3 }}>
       <CardContent sx={{ display: 'flex', gap: 1 }}>
@@ -31,6 +38,7 @@ export const DirectionPerson: FC<IDirectionPersonProps> = ({ image, job, name, b
             variant="h5"
             textAlign='justify'
             paddingBottom={1}
+            fontSize={(smDown && mdDown && lgDown && xlDown && !xsDown) ? 16 : undefined}
             sx={{
               minHeight: '3rem',
               maxHeight: '6rem',
@@ -43,6 +51,7 @@ export const DirectionPerson: FC<IDirectionPersonProps> = ({ image, job, name, b
             variant="h5"
             color="text.secondary"
             textAlign='justify'
+            fontSize={(smDown && mdDown && lgDown && xlDown && !xsDown) ? 16 : undefined}
             sx={{
               minHeight: '3rem',
               maxHeight: '6rem',
